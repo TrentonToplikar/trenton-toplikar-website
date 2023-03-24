78a68009710b2ac209758df09151cbbd181eb015
TrentonToplikar <ttoplikar@gmail.com>
2023-03-23 20:00:09 -0700

    fixed button

--- src/components/DownArrow.jsx
+++ src/components/DownArrow.jsx
@@ -3,7 +3,7 @@ import './styles/DownArrow.css'
 export const DownArrow = () => {
     return (
         <div className="down-arrow-container">
-            <a href="#about-me">
+            <a href="#about">
             <img className="down-arrow-img" src="https://cdn4.iconfinder.com/data/icons/essentials-71/24/022_-_Arrow-512.png" alt="down-arrow" />
             </a>
         </div>
